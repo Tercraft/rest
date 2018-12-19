e8c67ef88996ca55f3ac9dc3868ce5af2cb77079
Yan Smir <bbatun@mail.ru>
2018-12-19 20:05:59 +0800

    dayz

---
+++ zadanye3.php
@@ -0,0 +1,30 @@
+<?php
+//Здадание 3
+$day = 4;
+
+switch ($day) {
+    case $day >1 && $day < 5:
+      echo "Это рабочий день";
+        break;
+    case $day >= 6 && $day <= 7;
+      echo "Это выходной";
+        break; 
+    default:
+      echo "Неизвестный день";
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ zadanye4.php
@@ -0,0 +1,32 @@
+<?php
+//Задание 4
+
+$bmw = [
+    "model" => "X5",
+    "speed" => 120,
+    "doors" => 5,
+    "year" => "2006",
+];
+
+$toyota = [
+    "model" => "Carina",
+    "speed" => 130,
+    "doors" => 4,
+    "year" => "2007",
+];
+
+$opel = [
+    "model" => "Corsa",
+    "speed" => 140,
+    "doors" => 5,
+    "year" => "2007",
+];
+
+echo "$bmw[model] - $bmw[speed] - $bmw[doors] - $bmw[year]";
+echo "</br>";
+
+echo "$toyota[model] - $toyota[speed] - $toyota[doors] - $toyota[year]";
+echo "</br>";
+
+echo "$opel[model] - $opel[speed] - $opel[doors] - $opel[year]";
+echo "</br>";
