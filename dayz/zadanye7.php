4479afdec3b6db2a950c591300c4808b98e08961
Pimpys <maxkyivua@gmail.com>
2019-02-02 14:06:03 +0200

    edit

--- dayz/zadanye7.php
+++ dayz/zadanye7.php
@@ -1,8 +1,7 @@
-<header>
-        
+
 <?php
 
-$meny = [
+$menu = [
     'news.php' => 'Главная',
     '#' => 'О нас',
     '#' => 'Курсы',
@@ -11,38 +10,31 @@ $meny = [
     'contact.php' => 'Контакты',
 ];
 
-function getMeny(array $meny, $vertical = false)
-{
-    echo '<nav class="grid">';
-    
-    foreach ($meny as $key => $value) {
-        echo '<a href="$key"><div class="nav__item grid">$value</div></a>';
-    }
-    
-    echo '</nav>';
-}
+//function getMenu(array $menu, $vertical = false)
+//{
+//    echo '<nav class="grid">';
+//    
+//    foreach ($menu as $key => $value) {
+//        echo '<a href="'. $key .'"><div class="nav__item grid">' . $value . '</div></a>';
+//    }
+//    
+//    echo '</nav>';
+//}
 
-getMeny($meny);
-
-function getMeny(array $meny, $vertical = false)
+getMenu($menu);
+//
+function getMenu(array $menu, $vertical = false)
 {
      if ($vertical){
          
          echo '<div class = "right__article__container">';
      }
      
-     else[
+     else
          echo '<nav class="grid">';
     
-     foreach ($meny as $key => $value) {
-        echo '<a href = '$key' class = 'item'>$value</a>';
+     foreach ($menu as $key => $value) {
+        echo '<a href = ' . $key . ' class = item>' . $value  . '</a>';
     }
     echo '</nav>';
-     ]
 }
-
-
-  
-        
-        
-        
